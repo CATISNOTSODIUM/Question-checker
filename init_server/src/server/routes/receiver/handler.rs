108a8eb016057ad10d2a97d67ce44ecded6e1ea9
Coffee Cat <thitiwatipst@gmail.com>
2024-05-21 23:16:09 +0700

    add error handling system

--- init_server/src/server/routes/receiver/handler.rs
+++ init_server/src/server/routes/receiver/handler.rs
@@ -1,17 +1,28 @@
 use std::fs::File;
 use std::io::Read;
+
 //internal library
 use super::response_type::{MyBareResponse, MyResponse, MyBody, QuestionType};
 
 
-pub fn convert_format(input: &MyBareResponse) -> Result<MyResponse, String> {
+pub fn convert_format(input: &MyBareResponse) -> Result<MyResponse, Box<dyn std::error::Error>> {
 
     //Check question-id format
     let parts: Vec<_> = input.question_id.split('_').collect();
     if parts.len() != 4{
-        return Err("Invalid question id".to_owned());
+        return Err("Invalid question id")?;
     }
     
+    match parts[1].parse::<u32>() {
+        Ok(_) => (),
+        Err(_) => {return Err("Invalid skillset id")?;},
+    }
+
+    match parts[2].parse::<u32>() {
+        Ok(_) => (),
+        Err(_) => {return Err("Invalid skill id")?;},
+    }
+
     //check question type
 
     let question_type_list_string = vec!["MCQ".to_string(), "TFQ".to_string(), "CAT".to_string(), "ERC".to_string()];
@@ -19,7 +30,7 @@ pub fn convert_format(input: &MyBareResponse) -> Result<MyResponse, String> {
     let question_type_index = question_type_list_string.clone()
     .iter()
     .position(|r| r == input.question_type.as_str())
-    .expect("Invalid question type");
+    .ok_or("Invalid question type. Available question types: MCQ, TFQ, CAT, ERC")?;
 
 
     Ok(MyResponse {
@@ -33,15 +44,15 @@ pub fn convert_format(input: &MyBareResponse) -> Result<MyResponse, String> {
 }
 
 
-pub async fn get_answer(file_name:String) -> Vec<MyResponse> {
-    let root = project_root::get_project_root().unwrap();
+pub async fn get_answer(file_name:String) -> Result<Vec<MyResponse>, Box<dyn std::error::Error>> {
+    let root = project_root::get_project_root()?;
     //read file .json
     let file_name = file_name;
     let target_dir = root.join("assets/questions").join(file_name.as_str());
-    let mut file = File::open(target_dir).expect("Cannot open file");
+    let mut file = File::open(target_dir)?;
     let mut data = String::new();
-    file.read_to_string(&mut data).expect("Invalid JSON");
-
-    let answers_list : Vec<MyResponse> = serde_json::from_str(data.as_str()).unwrap();
-    answers_list
+    file.read_to_string(&mut data)?;
+    let answers_list : Vec<MyResponse> = serde_json::from_str(data.as_str())?;
+    
+    Ok(answers_list)
 }
--- init_server/src/server/routes/receiver/mod.rs
+++ init_server/src/server/routes/receiver/mod.rs
@@ -1,43 +1,53 @@
 pub mod response_type;
 pub mod handler;
 
-use axum::Json;
+use axum::{Json,
+    response::IntoResponse,
+    http::StatusCode};
 use response_type::MyBareResponse;
 use handler::{convert_format, get_answer};
 
 
 pub async fn check_answer(
 //parse JSON input into struct MyBareResponse
-Json(payload): Json<MyBareResponse>,) -> String 
+Json(payload): Json<MyBareResponse>,) -> impl IntoResponse
 { 
-    //Struct validation: convert MyBareResponse -> MyResposne
-    let response = convert_format(&payload).unwrap();
-    
+   match handler(payload).await {
+    //Successful response
+    Ok(res) => (StatusCode::OK, res.to_owned()).into_response(),
+    //For unsuccessful response
+    Err(err) => {
+        return (StatusCode::INTERNAL_SERVER_ERROR, format!("Process failed: {}", &err)).into_response();
+    }
+   }
+}
+
+pub async fn handler(payload: MyBareResponse) -> Result<String, Box<dyn std::error::Error>>  {
+     //Struct validation: convert MyBareResponse -> MyResposne
+    let response = convert_format(&payload)?;
+
     //retrieve the answer key (which is a list of all questions)
-    let answers_list = get_answer("answer.json".to_owned()).await;
+    let answers_list = get_answer("answer.json".to_owned()).await?;
 
     //Brute force finding the question that has the same id with response.  (which is bad)
 
     let target_answer_index = answers_list.iter()
     .position(|ans| ans.question_id == response.clone().question_id)
-    .expect("cannot find answer"); 
+    .ok_or("Cannot find the actual answer from database.")?; 
 
     let target_answer = &answers_list[target_answer_index];
 
     //check question type
     if target_answer.clone().question_type !=  response.clone().question_type{
-        return "INVALID question TYPE".to_string();
+        return Err("According to the question-id, the expected question type for this response format is not the same as the question type identified in the database.")?;
     }
 
     //check answer
     if target_answer.clone().body.answer == response.clone().body.answer
     && target_answer.clone().body.answer_index ==  response.clone().body.answer_index
     {
-        "CORRECT".to_string()
+        Ok("CORRECT".to_string())
     } else {
-        "WRONG".to_string()
+        Ok("WRONG".to_string())
     }
-
 }
-
-
